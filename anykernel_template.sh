48b6f70b5076883d7f399d963f7fb8bca64313df
xCaptaiN09 <dilsheddilu123@gmail.com>
2026-03-05 11:45:06 +0530

    ci: add GitHub Actions build workflow for lineage-23.2

---
+++ anykernel_template.sh
@@ -0,0 +1,43 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=SukiSU-Ultra-Definitive-xCaptaiN09
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=RMX3031
+device.name2=RealmeX7Max
+device.name3=cupid
+device.name4=mt6893
+device.name5=
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; } # end properties
+
+### AnyKernel install
+## boot files attributes
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $RAMDISK/*;
+set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
+} # end attributes
+
+# boot shell variables
+BLOCK=/dev/block/by-name/boot;
+IS_SLOT_DEVICE=0;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+# boot install
+split_boot;
+
+flash_boot;
+## end boot install
